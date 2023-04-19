1cfa5fdbcd5be3aa3295acbfd47c289413daae4d
shwetabh1920 <80441544+shwetabh1920@users.noreply.github.com>
2023-04-19 20:27:15 +0530

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+from textblob import TextBlob
+st.title('Sentiment Analysis')
+st.write('Enter the text to analyze below:')
+text = st.text_input('Text Input')
+if st.button('Analyze Sentiment'):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    if sentiment > 0:
+        st.write('Positive')
+    elif sentiment < 0:
+        st.write('Negative')
+    else:
+        st.write('Neutral')
